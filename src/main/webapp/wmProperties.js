82bb61cb7175814154693fd68426a9e8a7aeec3d
root <root@7f811032b533>
2016-08-25 11:51:02 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "defaultLanguage" : "en",
+  "displayName" : "TestDB_HSQL_rolodex1",
+  "homePage" : "Main",
+  "name" : "TestDB_HSQL_rolodex1",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
